ee0df966c48513776bf73f640420b07894b116cb
bobimel2nd <31457563+bobimel2nd@users.noreply.github.com>
2017-12-09 22:17:11 -0800

    Version 1.1.0

--- assets/movieSearch.js
+++ assets/movieSearch.js
@@ -1,109 +1,133 @@
 $( document ).ready(function() {
   var movie = $("#movie");
-  var pg = 1;
-  var tp = 1;
+  var Movies = [];
+  var displayPage = 1;
+  var pageDisplayed = 0;
+  var totalPages = 1;
+  var columnsPerPage = 6;
+  var columnSize = "col-xs-2";
+  var rowsPerPage = 3;
+
+  $(document).on("click", "#Search", function (){
+    searchMovies(movie.val());
+  });
+  $(document).on('keyup', "#movie", function (e) {
+      if (e.keyCode == 13) {
+         searchMovies(movie.val());
+      }
+  });
+  $(document).on("click", ".btn", function() {
+    ChangePage($(this).text());
+  });
+
+  $(document).on("click", ".smallPic", function() {
+      GetMovie($(this).data("imdbID"));
+  });
 
   function ChangePage(page) {
     if (page === "«") {
-      pg = 1;
-    } else if (page === "&lt;") {
-      if (pg > 1) pg--;
-    } else if (page === "&gt;") {
-      if (pg < tp) pg++;
+      displayPage = 1;
+    } else if (page === "<") {
+      if (displayPage > 1) displayPage--;
+    } else if (page === ">") {
+      if (displayPage < totalPages) displayPage++;
     } else if (page === "»") {
-      pg = tp;
+      displayPage = totalPages;
     } else {
-      pg = parseInt(page);
+      displayPage = parseInt(page);
     }
-    SearchMovie(movie.val(), pg);
+    updateDisplay();
+    updateButtons();
   }
 
-  $("#Search").on("click", function (){
-    pg = 1;
-    SearchMovie(movie.val(), pg);
-  });
-  $("#movie").on('keyup', function (e) {
-      if (e.keyCode == 13) {
-        pg = 1;
-        SearchMovie(movie.val(), pg);
-      }
-  });
+  function searchMovies(title) {
+    var title = title.replace(" ", "+");
+    var title = title.replace(":", "+");
+    var title = title.replace("-", "+");
+    var title = title.replace(",", "+");
+    displayPage = 1;
+    pageDisplayed = 0;
+    totalPages = 1;
+    Movies = [];
+    getAllMovies(title, 1);
+  }
 
-  function SearchMovie(title, page) {
-    var title = title.replace(" ", "+")
-    var title = title.replace(":", "+")
-    var title = title.replace("-", "+")
+  function getAllMovies(title, page) {
     var queryURL = "https://www.omdbapi.com/?s=" + title + "&page=" + page + "&plot=short&r=json&apikey=trilogy";
-
-    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
-      // for each item returned
-      for (i=0; i<10; i++) {
-        var r = Math.floor(i/2);
-        var c = (i%2)*5;
-        var rowTds = $("table")
-          .children()
-          .eq(1)
-          .children("tr")
-          .eq(r)
-          .children("td");
-        if (i < response.Search.length) {
-          // Setting the inner text of each td in the first row
-          rowTds.eq(c+0).html('<img src="' + response.Search[i].Poster + '" alt="' + response.Search[i].imdbID + '" class="smallPic" height="50">');
-          rowTds.eq(c+1).text(response.Search[i].Year);
-          rowTds.eq(c+2).text(response.Search[i].Type);
-          rowTds.eq(c+3).text(response.Search[i].Title);
-        } else {
-          rowTds.eq(c+0).html('');
-          rowTds.eq(c+1).text('');
-          rowTds.eq(c+2).text('');
-          rowTds.eq(c+3).text('');
-        }
+    $.ajax({ 
+      url: queryURL, 
+      method: "GET" })
+    .done(function(response) {
+      for (i=0; i<response.Search.length; i++) {
+        Movies.push(response.Search[i]);
       }
-      // Total Pages
-      tp = Math.floor(response.totalResults/10);
-      if (response.totalResults%10 > 0) tp++;
+      updateDisplay();
+      updateButtons();
+      if (response.totalResults > Movies.length) getAllMovies(title, page+1);
+    })
+  }
 
-      var n=10;
-      if (tp < 10) n = tp;
-      var s=page;
-      var e=page+n;
-      if (e > tp) {
-        s = tp-n;
-        if (s<1) s=1;
-        e = tp;
+  function updateButtons(){
+    var n = 10;
+    var totalRows = Math.floor((Movies.length-1)/columnsPerPage)+1;
+    var beg = displayPage;
+    var end = displayPage + n;
+    totalPages = Math.floor((totalRows)/rowsPerPage);
+    if (end > totalPages) {
+      beg = totalPages - n;
+      if (beg < 1) beg = 1;
+      end = totalPages;
+    }
+    var pages = '<a href="#" class="btn btn-primary">&laquo;</a>';
+    pages += '<a href="#" class="btn btn-primary">&lt;</a>';
+    for (i=beg; i<=end; i++) {
+      if (i === displayPage) {
+        pages += '<a href="#" class="btn btn-primary bolded">' + i + '</a>';
+      } else {
+        pages += '<a href="#" class="btn btn-primary">' + i + '</a>';
       }
-      var pages = '<a href="#" class="btn btn-primary">&laquo;</a>';
-      pages += '<a href="#" class="btn btn-primary">&lt;</a>';
-      for (i=s; i<=e; i++) {
-        if (i === page) {
-          pages += '<a href="#" class="btn btn-primary yellow">' + i + '</a>';
-        } else {
-          pages += '<a href="#" class="btn btn-primary">' + i + '</a>';
-        }
+    }
+    pages += '<a href="#" class="btn btn-primary">&gt;</a>';
+    pages += '<a href="#" class="btn btn-primary">&raquo;</a>';
+    $("#buttons").html(pages);
+  }
+
+  function updateDisplay() {
+    if (pageDisplayed === displayPage) return;
+    var beg = ((displayPage-1)*columnsPerPage*rowsPerPage);
+    var end = (displayPage*columnsPerPage*rowsPerPage);
+    if (i >= Movies.length) return;
+    var html = $("#coverRows");
+    html.html("");
+    for (i=beg; i<end; i++) {
+      if (i >= Movies.length) break;
+      if ((i % columnsPerPage) === 0) {
+        var row = $("<div>");
+        row.addClass("row");
+        html.append(row);
       }
-      pages += '<a href="#" class="btn btn-primary">&gt;</a>';
-      pages += '<a href="#" class="btn btn-primary">&raquo;</a>';
-      $("#buttons").html(pages);
-
-      $('.btn').on("click", function() {
-        ChangePage($(this).html());
-      });
-
-      // When the user clicks on an image, open the modal 
-      $(".smallPic").on("click", function() {
-          GetMovie($(this).attr("alt"));
-          DataPopupOpen("moviePopup")
-      });
-    });
+      var col = $("<div>");
+      col.addClass(columnSize)
+      row.append(col);
+      var img = $("<img>");
+      img.addClass("smallPic");
+      img.attr("src", Movies[i].Poster);
+      img.attr("alt", Movies[i].Title + " [" + Movies[i].Year + "]");
+      img.attr("title", Movies[i].Title + " [" + Movies[i].Year + "]");
+      img.data("imdbID",Movies[i].imdbID)
+      col.append(img);
+      }
+    if (i === end ) pageDisplayed = displayPage;
   }
 
   function GetMovie(imdbID) {
     var queryURL = "https://www.omdbapi.com/?i=" + imdbID + "&y=&plot=Full&apikey=trilogy";
-
     $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
       $("#poster").attr("src", response.Poster);
       $("#title").text(response.Title + ' (' + response.Year + ')');
       var details = '';
+      details += 'Type: ' + response.Type;
+      details += '<br>';
       details += 'Rated: ' + response.Rated;
       details += '<br>';
       details += 'Released: ' + response.Released;
@@ -116,6 +140,7 @@ $( document ).ready(function() {
       details += '<br><br>';
       details += response.Plot;
       $("#details").html(details)
+      DataPopupOpen("moviePopup");
     });
   };
 })
